050d59376f1d67f854949311a43bfbe64095b7f6
Zevi Liao <jointjm@gmail.com>
2020-09-26 14:44:31 +0800

    with bs3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -31,7 +31,7 @@ export class AppComponent implements OnInit {
 
   public msg: string;
   ngOnInit() {
-    $('[data-toggle="popover"]').popover();
+    // $('[data-toggle="popover"]').popover();
   }
 
   callApi() {
