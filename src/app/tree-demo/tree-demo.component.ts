954034284d10368fd3ad429f4f95298107520457
Zevi Liao <jointjm@gmail.com>
2020-09-27 21:52:21 +0800

    get nodeIds

--- src/app/tree-demo/tree-demo.component.ts
+++ src/app/tree-demo/tree-demo.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
 import { ITreeOptions, TreeModel, TreeNode } from '@circlon/angular-tree-component';
 
 @Component({
@@ -6,44 +6,97 @@ import { ITreeOptions, TreeModel, TreeNode } from '@circlon/angular-tree-compone
   templateUrl: './tree-demo.component.html',
   styleUrls: ['./tree-demo.component.scss']
 })
-export class TreeDemoComponent {
+export class TreeDemoComponent implements AfterViewInit {
+
+  @ViewChild('tree') treeComponent;
 
   options: ITreeOptions = {
     useCheckbox: true
   };
   nodes = [
     {
+      id: 1,
       name: 'North America',
       children: [
-        { name: 'United States', children: [
-          {name: 'New York'},
-          {name: 'California'},
-          {name: 'Florida'}
-        ] },
-        { name: 'Canada' }
+        {
+          id: 11,
+          name: 'United States', children: [
+            {
+              id: 111,
+              name: 'New York'
+            },
+            {
+              id: 112,
+              name: 'California'
+            },
+            {
+              id: 113,
+              name: 'Florida'
+            }
+          ]
+        },
+        {
+          id: 12,
+          name: 'Canada'
+        }
       ]
     },
     {
+      id: 2,
       name: 'South America',
       children: [
-        { name: 'Argentina', children: [] },
-        { name: 'Brazil' }
+        {
+          id: 21,
+          name: 'Argentina', children: []
+        },
+        {
+          id: 22,
+          name: 'Brazil'
+        }
       ]
     },
     {
+      id: 3,
       name: 'Europe',
       children: [
-        { name: 'England' },
-        { name: 'Germany' },
-        { name: 'France' },
-        { name: 'Italy' },
-        { name: 'Spain' }
+        {
+          id: 31,
+          name: 'England'
+        },
+        {
+          id: 32,
+          name: 'Germany'
+        },
+        {
+          id: 33,
+          name: 'France'
+        },
+        {
+          id: 34,
+          name: 'Italy'
+        },
+        {
+          id: 35,
+          name: 'Spain'
+        }
       ]
     }
   ];
 
   filterFn(value: string, treeModel: TreeModel) {
     treeModel.filterNodes((node: TreeNode) => fuzzysearch(value, node.data.name));
+  }
+
+  ngAfterViewInit() {
+    this.treeComponent.treeModel.subscribeToState((state: any) => { // ITreeState does not contain selectedLeafNodeIds
+
+      console.log(state);
+      console.log(this.treeComponent.treeModel.selectedLeafNodeIds);
+
+      // drop the unwanted prototypes
+      const selected = Object.assign({}, this.treeComponent.treeModel.selectedLeafNodeIds);
+      console.log(selected);
+    });
   }
 
 }
